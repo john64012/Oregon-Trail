de93f474998b15524dd3621fc3e2ebf91af38e47
Jamie Mortensen <16mortensenj@gmail.com>
2022-04-03 18:46:56 -0400

    renamed person file

---
+++ src/person.py
@@ -0,0 +1,28 @@
+
+
+from __future__ import annotations
+import abc
+
+import random
+
+
+class Person():
+    def __init__(self, weight:int, money:int, strength:int) -> None:
+        self.weight = weight
+        self.money = money
+        self.strength = strength
+
+    @classmethod
+    def random(cls, weight_range:list[int], money_range:list[int],strength_range:list[int]):
+        weight = random.randrange(*weight_range)
+        money = random.randrange(*money_range)
+        strength = random.randrange(*strength_range)
+        return cls(weight,money,strength)
+
+
+
+
+class PersonDefaultSettings():
+    WEIGHT_RANGE = [100,150]
+    STRENGTH_RANGE = [1,10]
+    MONEY_RANGE = [25,150]
